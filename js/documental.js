129b10379bb4cddfdf04caaa840498fb77663bbe
HuellasDeVida <39761868+HuellasDeVida@users.noreply.github.com>
2018-07-15 01:41:09 -0500

    new videos on map section

--- js/documental.js
+++ js/documental.js
@@ -1,4 +1,4 @@
-var acercamiento = 10;
+
 
 function closeWaitScreen() {
   $('.wait-screen').css('display', 'none');
@@ -18,8 +18,8 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiaGVuZGVsY3VhcnRhcyIsImEiOiJjamlveDZ3YTUwdW91M
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v10',
-  zoom: 20,
-  center: [-77.00544781668299, 3.8711397708862423],
+  zoom: 14,
+  center: [-77.01440794068435, 3.8741092616790826],
 });
 
 const geojson = {
@@ -77,6 +77,149 @@ const geojson = {
       properties: {
         title: 'Sixta Zambrano',
         url: 'https://www.youtube.com/embed/irdFkdVOwUY'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-77.00931917162126, 3.8742967394096306]
+      },
+      properties: {
+        title: 'María Ortega',
+        url: 'https://www.youtube.com/embed/KFHmzaTCnbk'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-77.00958007767514, 3.8743708488698445]
+      },
+      properties: {
+        title: 'Silvia Ortega',
+        url: 'https://www.youtube.com/embed/FbTg3YJgRg8'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-77.009414755931, 3.8743659070905783]
+      },
+      properties: {
+        title: 'Silvia y María Ortega, Stella Cuero',
+        url: 'https://www.youtube.com/embed/0Pd1eDaRtnU'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-77.02245918213856, 3.8738132672310837]
+      },
+      properties: {
+        title: 'Parturienta',
+        url: 'https://www.youtube.com/embed/h8Ds3zBb1s8'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-77.02253282080045, 3.8737615967159753]
+      },
+      properties: {
+        title: 'María Peña',
+        url: 'https://www.youtube.com/embed/OaW-pVNqz0E'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-77.02249685478245, 3.873752333045005]
+      },
+      properties: {
+        title: 'María Peña',
+        url: 'https://www.youtube.com/embed/F5eznXyjyj8'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-77.02244479551035, 3.873771168224894]
+      },
+      properties: {
+        title: 'María Peña',
+        url: 'https://www.youtube.com/embed/GdndTwAS060'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-77.02249815017302, 3.8738256738827315]
+      },
+      properties: {
+        title: 'María Peña',
+        url: 'https://www.youtube.com/embed/-SbejEa6KWQ'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-77.02252509416502, 3.873815081685012]
+      },
+      properties: {
+        title: 'María Peña',
+        url: 'https://www.youtube.com/embed/HeXy3VL64kY'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-77.00531315926901, 3.871085559757514]
+      },
+      properties: {
+        title: 'Partera Graciela',
+        url: 'https://www.youtube.com/embed/n-hObyQO544'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-77.00530014445098, 3.871119401449661]
+      },
+      properties: {
+        title: 'María Peña',
+        url: 'https://www.youtube.com/embed/hY02chp85Rs'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-77.00529048239423, 3.8711458838949104]
+      },
+      properties: {
+        title: 'Melina Hernández',
+        url: 'https://www.youtube.com/embed/qWoPKk10WbQ'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-77.00527350521361, 3.871176102025013]
+      },
+      properties: {
+        title: 'Cerafina Castillo',
+        url: 'https://www.youtube.com/embed/VLVjfKzssYs'
       }
     }
   ]
